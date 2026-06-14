4cfe9bfd3a4826aa18d3206f4ef6f36a5f06a610
jjwall101-lgtm <jjwall101@hotmail.co.uk>
2026-06-14 10:01:56 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
-const CACHE='adhd-health-os-v41-fixed';
-const FILES=['./','./index.html','./styles.css','./app.js','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+const CACHE='adhd-health-os-v42-working';
+self.addEventListener('install',e=>{self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
